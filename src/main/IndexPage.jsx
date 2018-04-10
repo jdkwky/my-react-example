3b8aca352e105105ac06b8cc22cf666b55a6a901
wkyyc <wkyyc86@gmail.com>
2018-04-10 08:28:13 +0800

    修改首页样式

--- src/main/IndexPage.jsx
+++ src/main/IndexPage.jsx
@@ -3,6 +3,8 @@ import { Layout, Menu, Breadcrumb } from 'antd';
 const { Header, Content, Footer } = Layout;
 import EditTableDemo from 'editTable/EditTableDemo';
 import TimelineDemo from 'timeline/TimelineDemo';
+import styles from './indexPage.less';
+
 const keyMap = {
   editTable: { component: <EditTableDemo />, breadcrumb: 'editTable' },
   timeline: { component: <TimelineDemo />, breadcrumb: 'timeline' }
@@ -23,8 +25,8 @@ class IndexPage extends Component {
     const { component, breadcrumb } = keyMap[menu];
 
     return (
-      <Layout>
-        <Header style={{ position: 'fixed', width: '100%' }}>
+      <div style={{ height: '100%' }}>
+        <div style={{ position: 'fixed', width: '100%', height: 64 }}>
           <Menu
             theme="dark"
             mode="horizontal"
@@ -35,15 +37,15 @@ class IndexPage extends Component {
             <Menu.Item key="editTable">可编辑表格</Menu.Item>
             <Menu.Item key="timeline">可点击时间轴</Menu.Item>
           </Menu>
-        </Header>
-        <Content style={{ padding: '0 50px', marginTop: 64 }}>
-          <Breadcrumb style={{ padding: '0 50px' }}>
+        </div>
+        <div className={styles.content}>
+          <Breadcrumb style={{ padding: '10px 50px' }}>
             <Breadcrumb.Item>{breadcrumb}</Breadcrumb.Item>
           </Breadcrumb>
-          <div style={{ background: '#fff', padding: 24, minHeight: 800 }}>{component}</div>
-        </Content>
-        <Footer style={{ textAlign: 'center' }}>wky &copy; 2018-04-09</Footer>
-      </Layout>
+          <div className={styles.realContent}>{component}</div>
+        </div>
+        <div className={styles.footer}>wky &copy; 2018-03-26</div>
+      </div>
     );
   }
 }
