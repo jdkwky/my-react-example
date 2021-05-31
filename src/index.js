d8335702bcef9f87dd4326478c2b6ddd4ffd509f
wangkaiyuan <wangkaiyuan@kuaishou.com>
2021-05-31 20:40:45 +0800

    update : 支持 spa

--- src/index.js
+++ src/index.js
@@ -6,29 +6,54 @@ import TimelineDemo from 'timeline/TimelineDemo';
 import registerServiceWorker from './registerServiceWorker';
 import IndexPage from 'main/IndexPage';
 import printMe from './print.js';
+import singleSpaReact from 'single-spa-react';
 
-console.log('11111111111111');
+// console.log('11111111111111');
 
-console.log(module.hot, 'module-hot');
+// console.log(module.hot, 'module-hot');
 
-ReactDOM.render(
-  <div>
-    <IndexPage />
-  </div>,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//     <div>
+//         <IndexPage />
+//     </div>,
+//     document.getElementById('root')
+// );
+/**start 微服务 */
+// 微服务
+// 导出生命周期
 
-registerServiceWorker();
-
-
-
-if (module.hot) {
-  module.hot.accept('./print.js', function () {
-    console.log('Accepting the updated printMe module!');
-    printMe();
-  })
-  module.hot.accept('./editTable/component/EditableCell.jsx', function () {
-    console.log('Accept the update EditTable module!');
-
-  })
+if (!window.singleSpaNavigate) {
+    // 如果不是single-spa模式 test
+    ReactDOM.render(
+        <div>
+            <IndexPage />
+        </div>,
+        document.getElementById('root')
+    );
 }
+const lifecycles = singleSpaReact({
+    React,
+    ReactDOM,
+    rootComponent: () => <IndexPage />,
+    errorBoundary(err, info, props) {
+        return <div> Error </div>;
+    },
+    domElementGetter: () => document.getElementById('vue'),
+});
+export const bootstrap = lifecycles.bootstrap;
+export const mount = lifecycles.mount;
+export const unmount = lifecycles.unmount;
+
+/** end 微服务 */
+
+// registerServiceWorker();
+/**  热更新 */
+// if (module.hot) {
+//     module.hot.accept('./print.js', function() {
+//         console.log('Accepting the updated printMe module!');
+//         printMe();
+//     });
+//     module.hot.accept('./editTable/component/EditableCell.jsx', function() {
+//         console.log('Accept the update EditTable module!');
+//     });
+// }
--- webpack.config.js
+++ webpack.config.js
@@ -1,128 +1,155 @@
 const path = require('path');
-const webpack = require("webpack");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const StatsPlugin = require('stats-webpack-plugin');
 
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 // const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
 //   .BundleAnalyzerPlugin;   分析打包文件
 
-module.exports = {
-  devtool: "eval-source-map",
-  entry: { bundle: ['@babel/polyfill', 'react-hot-loader/patch', __dirname + "/src/index.js"] },
-  output: {
-    path: __dirname + "/public",
-    filename: "[name]-[hash:5].js"
-  },
-  devServer: {
-    contentBase: "./public",
-    publicPath: "/",
-    historyApiFallback: true,
-    inline: true,
-    hot: true,
-    proxy: {
-      "/search/*": {
-        target: "https://image.baidu.com",
-        changeOrigin: true
-      }
-    }
-  },
-  module: {
-    rules: [
-      {
-        test: /(\.jsx|\.js)$/,
-        use: {
-          loader: "babel-loader",
-        },
-        exclude: /node_modules/
-      },
-      {
-        test: /(\.css|\.less)$/,
-        use: [
-          {
-            loader: "style-loader"
-          },
-          {
-            loader: "css-loader",
-            options: {
-              modules: true, // 指定启用css modules
-              localIdentName: "[name]__[local]--[hash:base64:5]" // 指定css的类名格式
-            }
-          },
-          {
-            loader: "less-loader"
-          }
-        ],
-        exclude: /node_modules/ //那些文件需要用上述loader
-      },
-      {
-        test: /(\.css|\.less)$/,
-        use: [
-          {
-            loader: "style-loader"
-          },
-          {
-            loader: "css-loader"
-          }
-        ],
-        exclude: /src/ //那些文件需要用上述loader
-      },
-      {
-        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
-        use: [
-          {
-            loader: 'url-loader',
-            options: {
-              limit: 10000,
-              outputPath: 'img'
-            }
-          }
-        ]
-      }
-    ]
-  },
-  optimization: {
-    splitChunks: {
-      chunks: 'all'
-    }
-  },
+const nodeEnv = process.env.NODE_ENV;
 
-  resolve: {
-    extensions: [".js", ".jsx"],
-    alias: {
-      editTable: __dirname + "/src/editTable",
-      timeline: __dirname + "/src/timeline",
-      main: __dirname + "/src/main",
-      waterfall: __dirname + "/src/waterfall",
-      canvas: __dirname + "/src/canvas",
-      test: __dirname + "/src/test",
-      utils: __dirname + "/src/utils",
-      demo: __dirname + "/src/demo"
-    }
-  },
-  plugins: [
-    new webpack.BannerPlugin("版权所有，翻版必究"),
+const defaultPlugins = [
+    new webpack.BannerPlugin('版权所有，翻版必究'),
     new webpack.optimize.OccurrenceOrderPlugin(),
     // new webpack.optimize.UglifyJsPlugin(),
-    new ExtractTextPlugin("style.css"),
-    new CleanWebpackPlugin({
-      root: __dirname + "/public",
-      cleanStaleWebpackAssets: false,
-      // exclude: ['assets'],
-      verbose: true // Write logs to console.
-    }),
+    new ExtractTextPlugin('style.css'),
 
     new HtmlWebpackPlugin({
-      filename: __dirname + "/public/index.html",
-      template: path.join(__dirname, 'src/index.html'),
-      title: 'my-react-example',
-      inject: true
+        filename: __dirname + '/public/index.html',
+        template: path.join(__dirname, 'src/index.html'),
+        title: 'my-react-example',
+        inject: true,
     }),
     // new BundleAnalyzerPlugin(),
     new webpack.NamedModulesPlugin(),
-    new webpack.HotModuleReplacementPlugin()
+    new webpack.HotModuleReplacementPlugin(),
+    new StatsPlugin('mainfest.json', {
+        chunkModules: false,
+        entrypoints: true,
+        source: false,
+        chunks: false,
+        modules: false,
+        assets: false,
+        children: false,
+        exclude: [/node_modules/],
+    }),
+];
 
-  ]
+const getPlugins = () => {
+    const plugins = [...defaultPlugins];
+    if (nodeEnv === 'package') {
+        plugins.push(
+            new CleanWebpackPlugin({
+                root: __dirname + '/public',
+                cleanStaleWebpackAssets: false,
+                // exclude: ['assets'],
+                verbose: true, // Write logs to console.
+            })
+        );
+    }
+    return plugins;
+};
+
+module.exports = {
+    devtool: 'eval-source-map',
+    entry: { bundle: ['@babel/polyfill', 'react-hot-loader/patch', __dirname + '/src/index.js'] },
+    output: {
+        path: __dirname + '/public',
+        filename: '[name]-[hash:5].js',
+        library: 'singleVue', // 导出名称
+        libraryTarget: 'window', //挂载目标
+        publicPath: '//localhost:8081/',
+    },
+    devServer: {
+        contentBase: './public',
+        publicPath: '/',
+        historyApiFallback: true,
+        inline: true,
+        hot: true,
+        proxy: {
+            '/search/*': {
+                target: 'https://image.baidu.com',
+                changeOrigin: true,
+            },
+        },
+        // changeOrigin: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /(\.jsx|\.js)$/,
+                use: {
+                    loader: 'babel-loader',
+                },
+                exclude: /node_modules/,
+            },
+            {
+                test: /(\.css|\.less)$/,
+                use: [
+                    {
+                        loader: 'style-loader',
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true, // 指定启用css modules
+                            localIdentName: '[name]__[local]--[hash:base64:5]', // 指定css的类名格式
+                        },
+                    },
+                    {
+                        loader: 'less-loader',
+                    },
+                ],
+                exclude: /node_modules/, //那些文件需要用上述loader
+            },
+            {
+                test: /(\.css|\.less)$/,
+                use: [
+                    {
+                        loader: 'style-loader',
+                    },
+                    {
+                        loader: 'css-loader',
+                    },
+                ],
+                exclude: /src/, //那些文件需要用上述loader
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 10000,
+                            outputPath: 'img',
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+    optimization: {
+        splitChunks: {
+            chunks: 'all',
+        },
+    },
+
+    resolve: {
+        extensions: ['.js', '.jsx'],
+        alias: {
+            editTable: __dirname + '/src/editTable',
+            timeline: __dirname + '/src/timeline',
+            main: __dirname + '/src/main',
+            waterfall: __dirname + '/src/waterfall',
+            canvas: __dirname + '/src/canvas',
+            test: __dirname + '/src/test',
+            utils: __dirname + '/src/utils',
+            demo: __dirname + '/src/demo',
+        },
+    },
+    plugins: getPlugins(),
 };
 // __dirname+'/js/newFeatureofEs6.js',__dirname+'/js/iteratorDemo.js',__dirname+'/js/test.js'
